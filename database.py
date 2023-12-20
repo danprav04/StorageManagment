454c2cc3ba3aefbf459b58b26a1b67b3b53eebff
danprav04 <62667601+danprav04@users.noreply.github.com>
2023-12-20 22:12:49 +0200

    one_session,, add_user

--- database.py
+++ database.py
@@ -8,3 +8,22 @@ engine = create_engine('sqlite:///StorageManagement.db')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
+
+
+def one_session(func):
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if isinstance(arg, Session):
+                return func(*args, **kwargs)
+        session = Session()
+        result = func(session, *args, **kwargs)
+        session.close()
+        return result
+    return wrapper
+
+
+@one_session
+def create_user(session, username, password, email):
+    new_user = User(username=username, password=password, email=email)
+    session.add(new_user)
+    session.commit()
