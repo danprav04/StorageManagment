8539fbde4680d4d2737cc6a061060106af70b34e
danprav04 <62667601+danprav04@users.noreply.github.com>
2023-12-20 22:21:27 +0200

    get users

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ Session = sessionmaker(bind=engine)
 def one_session(func):
     def wrapper(*args, **kwargs):
         for arg in args:
-            if isinstance(arg, Session):
+            if isinstance(arg, type(Session)):
                 return func(*args, **kwargs)
         session = Session()
         result = func(session, *args, **kwargs)
@@ -27,3 +27,11 @@ def create_user(session, username, password, email):
     new_user = User(username=username, password=password, email=email)
     session.add(new_user)
     session.commit()
+
+
+@one_session
+def get_users(session):
+    users = session.query(User).all()
+    for user in users:
+        print(f"ID: {user.id}, Username: {user.username}, Password: {user.password}, Email: {user.email}")
+
