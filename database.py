9d7e42c5de49244720a9aadc4b23b5a74a59b457
danprav04 <62667601+danprav04@users.noreply.github.com>
2023-12-20 23:06:58 +0200

    added models and database communication
    users
    storage units
    storage grids
    storage places

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 # models.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, User
+from models import Base, User, StoragePlace, StorageGrid, StorageUnit
 
 
 engine = create_engine('sqlite:///StorageManagement.db')
@@ -37,3 +37,145 @@ def create_user(session, username, password, email):
 def get_users(session):
     users = session.query(User).all()
     return users
+
+
+@one_session
+def get_user_by_id(session, user_id):
+    user = session.query(User).filter_by(id=user_id).first()
+    return user
+
+
+@one_session
+def delete_user_by_id(session, user_id):
+    user = session.query(User).filter_by(id=user_id).first()
+    if user:
+        session.delete(user)
+        session.commit()
+        return 'success'
+    else:
+        return 'User not found'
+
+
+# StoragePlace functions
+
+@one_session
+def create_storage_place(session, name, description, image):
+    try:
+        new_storage_place = StoragePlace(name=name, description=description, image=image)
+        session.add(new_storage_place)
+        session.commit()
+        return 'success'
+    except Exception as e:
+        return str(e)
+
+
+@one_session
+def get_storage_places(session):
+    storage_places = session.query(StoragePlace).all()
+    return storage_places
+
+
+@one_session
+def get_storage_place_by_id(session, storage_place_id):
+    storage_place = session.query(StoragePlace).filter_by(id=storage_place_id).first()
+    return storage_place
+
+
+@one_session
+def delete_storage_place_by_id(session, storage_place_id):
+    storage_place = session.query(StoragePlace).filter_by(id=storage_place_id).first()
+    if storage_place:
+        session.delete(storage_place)
+        session.commit()
+        return 'success'
+    else:
+        return 'Storage Place not found'
+
+
+# StorageGrid functions
+
+@one_session
+def create_storage_grid(session, row_count, column_count, description, image, storage_place_name):
+    session = Session()
+    storage_place = session.query(StoragePlace).filter_by(name=storage_place_name).first()
+
+    if storage_place:
+        new_storage_grid = StorageGrid(row_count=row_count, column_count=column_count, description=description, image=image, storage_place=storage_place)
+        session.add(new_storage_grid)
+        session.commit()
+        return 'success'
+    else:
+        return f"Storage Place '{storage_place_name}' not found."
+
+
+@one_session
+def get_storage_grids(session):
+    storage_grids = session.query(StorageGrid).all()
+    return storage_grids
+
+
+@one_session
+def get_storage_grid_by_id(session, storage_grid_id):
+    storage_grid = session.query(StorageGrid).filter_by(id=storage_grid_id).first()
+    return storage_grid
+
+
+@one_session
+def delete_storage_grid_by_id(session, storage_grid_id):
+    storage_grid = session.query(StorageGrid).filter_by(id=storage_grid_id).first()
+    if storage_grid:
+        session.delete(storage_grid)
+        session.commit()
+        return 'success'
+    else:
+        return 'Storage Grid not found'
+
+
+# StorageUnit functions
+
+@one_session
+def create_storage_unit(session, name, description, image, storage_grid_row, storage_grid_column, storage_place_name):
+    storage_place = session.query(StoragePlace).filter_by(name=storage_place_name).first()
+
+    if storage_place:
+        storage_grid = storage_place.storage_grids.first()
+        if storage_grid:
+            new_storage_unit = StorageUnit(
+                name=name,
+                description=description,
+                image=image,
+                storage_grid=storage_grid,
+                storage_grid_row=storage_grid_row,
+                storage_grid_column=storage_grid_column,
+                storage_place=storage_place
+            )
+            session.add(new_storage_unit)
+            session.commit()
+            return 'success'
+        else:
+            return f"No storage grid found for {storage_place_name}."
+    else:
+        return f"Storage Place '{storage_place_name}' not found."
+
+
+@one_session
+def get_storage_units(session):
+    storage_units = session.query(StorageUnit).all()
+    return storage_units
+
+
+@one_session
+def get_storage_unit_by_id(session, storage_unit_id):
+    storage_unit = session.query(StorageUnit).filter_by(id=storage_unit_id).first()
+    return storage_unit
+
+
+@one_session
+def delete_storage_unit_by_id(session, storage_unit_id):
+    storage_unit = session.query(StorageUnit).filter_by(id=storage_unit_id).first()
+    if storage_unit:
+        session.delete(storage_unit)
+        session.commit()
+        return 'success'
+    else:
+        return 'Storage Unit not found'
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -11,3 +11,36 @@ class User(Base):
     username = Column(String, unique=True)
     password = Column(String)
     email = Column(String, unique=True)
+
+
+class StoragePlace(Base):
+    __tablename__ = 'storage_places'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(Text)
+    image = Column(String)
+
+
+class StorageGrid(Base):
+    __tablename__ = 'storage_grids'
+
+    id = Column(Integer, primary_key=True)
+    row_count = Column(Integer, nullable=False)
+    column_count = Column(Integer, nullable=False)
+    description = Column(Text)
+    image = Column(String)
+    storage_place_id = Column(Integer, ForeignKey('storage_places.id'))
+
+
+class StorageUnit(Base):
+    __tablename__ = 'storage_units'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(Text)
+    image = Column(String)
+    storage_place_id = Column(Integer, ForeignKey('storage_places.id'))
+    storage_grid_id = Column(Integer, ForeignKey('storage_grids.id'))
+    storage_grid_row = Column(Integer, nullable=False)
+    storage_grid_column = Column(Integer, nullable=False)
